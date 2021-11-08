7fad25bf81d59e445d8bba495a831fb71eda89e4
mdeias <mdeias@icloud.com>
2021-11-08 22:36:39 +0100

    tentativo milestone 3

---
+++ milestone3/data.js
@@ -0,0 +1,161 @@
+const listaElementi = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+
+
+// **Milestone 3**
+// Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone *(animal, vegetable, user)*. Quando l’utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
+
+// creo un ciclo filter che mi restituisce i nomi e le icone
+const contenitore = document.querySelector(".container-icons");
+console.log(contenitore);
+
+listaElementi.forEach ((elemento) => {
+
+	
+	
+    const boxIcons = document.createElement("div");
+	boxIcons.classList.add("box-icona");
+	boxIcons.innerHTML = `
+	<i class="${elemento.family} ${elemento.prefix + elemento.name} ${elemento.color}"><i/>
+	<p class="nome"> ${elemento.name.toUpperCase()} <p/>
+	`;
+
+
+	// tentativo di risolvere il milestone3
+	document.getElementById('filtro').addEventListener("select",function(){
+		
+		let opzioni = parseInt(document.getElementById("filtro").value);
+		if (elemento.type === "user" && opzioni === 4 ) {
+			boxIcons.classList.add("active");
+		}else if (elemento.type == "vegetable" && opzioni === 3) {
+			boxIcons.classList.add("active");
+		}else if (elemento.type == "animal" && opzioni === 2) {
+			boxIcons.classList.add("active");
+		}
+		console.log(elemento.type);
+
+	})
+	
+	contenitore.append(boxIcons);
+	console.log(boxIcons);
+})
+
+
+
+
+
+
+
+
