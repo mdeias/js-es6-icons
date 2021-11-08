b8282d7e3f7da3f9b4759fde9d856b1d40a9eabc
mdeias <mdeias@icloud.com>
2021-11-08 17:30:16 +0100

    stampa box con nome, prefix e type

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-[
+const listaElementi = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
@@ -114,11 +114,29 @@
 ];
 
 
-                // **Milestone 1**
+// **Milestone 1**
 // Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell’icona e l’icona stessa.
 
-               // **Milestone 2**
+// creo un ciclo filter che mi restituisce i nomi e le icone
+const contenitore = document.querySelector(".container-icons");
+console.log(contenitore);
+
+listaElementi.forEach ((elemento) => {
+
+       const boxIcons = document.createElement("div");
+		boxIcons.classList.add("box-icona");
+		boxIcons.innerHTML = `
+		<span> ${elemento.prefix + elemento.type} </>
+		<span> ${elemento.name} </>
+		`;
+		
+		contenitore.append(boxIcons);
+})
+
+
+
+// **Milestone 2**
 // Ciascuna icona ha una proprietà “color”: utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
 
-                // **Milestone 3**
+// **Milestone 3**
 // Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone *(animal, vegetable, user)*. Quando l’utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
